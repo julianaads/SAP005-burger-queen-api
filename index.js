bb20c39807e30334e55efffa09dbf8bca5647e4d
Juliana <juliana.souzas@yahoo.com.br>
2021-03-21 09:53:45 -0300

    add config Heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const routes = require('./server/routes/index')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000;
 
 app.use('/', routes);
 
@@ -9,6 +9,8 @@ app.get('*', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// })
+
+app.listen(PORT);
