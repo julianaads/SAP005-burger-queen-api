4115432049479b0cb5c91a5df353cc317e03e486
Juliana <juliana.souzas@yahoo.com.br>
2021-03-23 11:16:23 -0300

    criando tabela no banco

--- server/controller/UsersController.js
+++ server/controller/UsersController.js
@@ -1,49 +1,71 @@
 // aqui vai o código que acessa o banco de dados
 
-const bdUsers = [
-  { id: 1, 
-    username: 'Carla Diaz', 
-    email: 'diazcarla@gmail.com',
-    role: 'cozinheira'
+const models = require('../db/models')
 
-},
-  { id: 2, 
-    username: 'João Luiz', 
-    email: 'jluiz@gmail.com',
-    role: 'atendente' 
-  }, 
-  { id: 3, 
-    username: 'Camila de Lucas', 
-    email: 'camiladl@gmail.com',
-    role:"cozinheira"
-  },
-  { id: 4, 
-    username: 'Juliete Freire', 
-    email: 'jufreire@gmail.com',
-    role: 'atendente',
-  }
-]
-
-
-const getAllUsers = (req, res) => {
-  res.send(bdUsers)
-}
-
-const getDeleteUsers = (req, res) => {
-let id = Number(req.params.id)
-  const newBdUsers = bdUsers.filter(data => data.id != id)
-  res.send(newBdUsers)
-  console.log(id)  
-}
-
-const getPostUser = (req, res) => {
-  const body = req.body;
-  if(!body){
-    res.status(400).end();
-  } else { 
-    bdUsers.push(body);
-    res.send(body);
+const findAll = async (req, res) => {
+  try {
+    const allUsers = await models.User.findAll({
+      raw: true,
+      attributes: {
+        exclude: ['password']
+      }
+    })
+    if (allUsers.length > 0) {
+      res.json(allUsers)
+    } else {
+      res.json({
+        message: "Nenhum usuário encontrado"
+      })
+    }
+  } catch (err) {
+    console.log(err)
   }
 }
 
-module.exports = { getAllUsers, getPostUser, getDeleteUsers}
+// const bdUsers = [
+//   { id: 1, 
+//     username: 'Carla Diaz', 
+//     email: 'diazcarla@gmail.com',
+//     role: 'cozinheira'
+
+// },
+//   { id: 2, 
+//     username: 'João Luiz', 
+//     email: 'jluiz@gmail.com',
+//     role: 'atendente' 
+//   }, 
+//   { id: 3, 
+//     username: 'Camila de Lucas', 
+//     email: 'camiladl@gmail.com',
+//     role:"cozinheira"
+//   },
+//   { id: 4, 
+//     username: 'Juliete Freire', 
+//     email: 'jufreire@gmail.com',
+//     role: 'atendente',
+//   }
+// ]
+
+
+// const getAllUsers = (req, res) => {
+//   res.send(bdUsers)
+// }
+
+// const getDeleteUsers = (req, res) => {
+// let id = Number(req.params.id)
+//   const newBdUsers = bdUsers.filter(data => data.id != id)
+//   res.send(newBdUsers)
+//   console.log(id)  
+// }
+
+// const getPostUser = (req, res) => {
+//   const body = req.body;
+//   if(!body){
+//     res.status(400).end();
+//   } else { 
+//     bdUsers.push(body);
+//     res.send(body);
+//   }
+// }
+
+module.exports = { findAll}
--- server/routes/Users.js
+++ server/routes/Users.js
@@ -4,8 +4,8 @@ const UsersController = require('../controller/UsersController')
 const router = Router()
 
 // aqui vai as requisições
-router.get("/", UsersController.getAllUsers);
-router.post("/", UsersController.getPostUser);
-router.delete("/:id", UsersController.getDeleteUsers);
+router.get("/", UsersController.findAll);
+// router.post("/", UsersController.getPostUser);
+// router.delete("/:id", UsersController.getDeleteUsers);
 
 module.exports = router
